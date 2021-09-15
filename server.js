4f1e66021f7aa7de6a67d04792332603047f97fc
afbolivarg <afbolivarg@gmail.com>
2021-09-14 23:24:51 -0500

    First author routes and searching

--- server.js
+++ server.js
@@ -18,7 +18,10 @@ app.use(express.urlencoded({ limit: '10mb', extended: false }))
 app.use(express.json());
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+mongoose.connect(process.env.DATABASE_URL, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+}).catch(error => console.error(error))
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
